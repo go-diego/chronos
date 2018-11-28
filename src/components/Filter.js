3c68a7f65f6d2330bfc00194f75cc0dc7fbf1367
Diego Bernal <diego91bernal@gmail.com>
2018-11-27 22:39:35 -0800

    feature: tag add filtering

---
+++ src/components/Filter.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default class Filter extends React.Component {
+    state = {
+        filterOptions: []
+    };
+
+    componentDidMount() {
+        const {options} = this.props;
+
+        const filterOptions = options.map(option => {
+            return {label: option, active: false};
+        });
+
+        this.setState({filterOptions});
+    }
+
+    handleToggleOption = index => {
+        const {filter} = this.props;
+        const filterOptions = [...this.state.filterOptions];
+        filterOptions[index].active = !filterOptions[index].active;
+        this.setState({filterOptions}, () => {
+            const options = filterOptions
+                .filter(option => option.active)
+                .map(option => option.label);
+            filter(options);
+        });
+    };
+
+    render() {
+        const {filterOptions} = this.state;
+
+        return (
+            <div className="tags">
+                {filterOptions.map((option, i) => {
+                    return (
+                        <span
+                            key={i}
+                            onClick={() => this.handleToggleOption(i)}
+                            className={`tag ${option.active ? "is-success" : "is-light"}`}>
+                            {option.label}
+                            {option.active && <button className="delete is-small" />}
+                        </span>
+                    );
+                })}
+            </div>
+        );
+    }
+}
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Filter from "../components/Filter";
+
 import Api from "../api/api";
 
 const api = new Api();
@@ -9,34 +11,76 @@ export default class Home extends React.Component {
     };
 
     async componentDidMount() {
-        const tags = await api.getTags();
-        console.log("TAGS", tags);
-
+        const response = await api.getTags();
+        const tags = response.map(tag => {
+            tag.isActive = true;
+            return tag;
+        });
         this.setState({tags});
     }
 
+    handleFilter = options => {
+        let tags = [...this.state.tags];
+
+        if (options.length === 0) {
+            tags.map(tag => {
+                tag.isActive = true;
+                return tag;
+            });
+        } else {
+            tags.forEach((tag, i) => {
+                const intersection = tag.features.filter(feature => options.includes(feature));
+                if (
+                    intersection.length > 0 &&
+                    JSON.stringify(options.sort()) === JSON.stringify(intersection.sort())
+                ) {
+                    tag.isActive = true;
+                } else {
+                    tag.isActive = false;
+                }
+            });
+        }
+
+        this.setState({tags});
+    };
+
     render() {
         const {tags} = this.state;
+        const uniqueFeatures = []
+            .concat(...tags.map(tag => tag.features))
+            .filter((tag, i, tags) => {
+                return tags.indexOf(tag) === i;
+            });
+
         return (
             <section className="section">
-                <h1 className="is-size-2">Tags</h1>
+                <div className="is-flex">
+                    <h1 className="is-size-2">Tags</h1>
+                    {uniqueFeatures.length > 0 && (
+                        <Filter filter={this.handleFilter} options={uniqueFeatures} />
+                    )}
+                </div>
                 <ul className="list">
                     {tags.map(tag => {
                         return (
-                            <li className="list-item" key={tag.tagId}>
-                                <div className="flex">
-                                    <p className="is-size-5"> {tag.label}</p>
-                                    <div className="tags">
-                                        {tag.features.map((feature, i) => {
-                                            return (
-                                                <span key={i} className="tag is-rounded is-warning">
-                                                    {feature}
-                                                </span>
-                                            );
-                                        })}
+                            tag.isActive && (
+                                <li className="list-item" key={tag.tagId}>
+                                    <div className="flex">
+                                        <p className="is-size-5"> {tag.label}</p>
+                                        <div className="tags">
+                                            {tag.features.map((feature, i) => {
+                                                return (
+                                                    <span
+                                                        key={i}
+                                                        className="tag is-rounded is-warning">
+                                                        {feature}
+                                                    </span>
+                                                );
+                                            })}
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
+                                </li>
+                            )
                         );
                     })}
                 </ul>
