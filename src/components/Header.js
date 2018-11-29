01e4dd51ed5c1efa002da6b20e0df2545a1d1feb
Diego Bernal <diego91bernal@gmail.com>
2018-11-28 16:52:39 -0800

    fix: navbar markup

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,20 +13,19 @@ export default function Header() {
                         height="28"
                     />
                 </Link>
-                <div className="navbar-menu">
-                    <div className="navbar-start" />
-                    <div className="navbar-end">
-                        <a
-                            className="navbar-item"
-                            target="_blank"
-                            href="https://github.com/go-diego/chronos">
-                            <img
-                                src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
-                                alt="Github Repository"
-                                width="60"
-                            />
-                        </a>
-                    </div>
+            </div>
+            <div className="navbar-menu">
+                <div className="navbar-end">
+                    <a
+                        className="navbar-item"
+                        target="_blank"
+                        href="https://github.com/go-diego/chronos">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+                            alt="Github Repository"
+                            width="60"
+                        />
+                    </a>
                 </div>
             </div>
         </nav>
